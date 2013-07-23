8f5562a235d2df3f705fbdf3675eb97d95d7fb08
silicon ruby <silicon@nachuantech.com>
2013-07-23 22:53:05 +0800

    Notification and Dialog

---
+++ NotificationTest/src/com/example/notificationtest/SecondActivity.java
@@ -0,0 +1,14 @@
+package com.example.notificationtest;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SecondActivity extends Activity {
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.next);
+	}
+
+}
