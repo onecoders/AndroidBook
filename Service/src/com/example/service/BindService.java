07318df1656dff22a70bdaeb22faede2c6697334
silicon ruby <silicon@nachuantech.com>
2013-09-01 18:06:06 +0800

    refs #1 Service and BroadcastReceiver
    Service

---
+++ Service/src/com/example/service/BindService.java
@@ -0,0 +1,54 @@
+package com.example.service;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.Binder;
+import android.os.IBinder;
+
+public class BindService extends Service {
+
+	private int count;
+	private boolean quit;
+
+	private MyBinder binder = new MyBinder();
+
+	public class MyBinder extends Binder {
+		public int getCount() {
+			return count;
+		}
+	}
+
+	@Override
+	public IBinder onBind(Intent arg0) {
+		return binder;
+	}
+
+	@Override
+	public void onCreate() {
+		super.onCreate();
+		new Thread() {
+			public void run() {
+				while (!quit) {
+					try {
+						Thread.sleep(1000);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+					count++;
+				}
+			};
+		}.start();
+	}
+
+	@Override
+	public boolean onUnbind(Intent intent) {
+		return super.onUnbind(intent);
+	}
+
+	@Override
+	public void onDestroy() {
+		super.onDestroy();
+		this.quit = true;
+	}
+
+}
---
+++ Service/src/com/example/service/BindServiceTest.java
@@ -0,0 +1,80 @@
+package com.example.service;
+
+import android.app.Activity;
+import android.app.Service;
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class BindServiceTest extends Activity {
+
+	private Button bind, unbind, getServiceStatus;
+	private BindService.MyBinder binder;
+
+	private ServiceConnection conn = new ServiceConnection() {
+
+		@Override
+		public void onServiceDisconnected(ComponentName name) {
+
+		}
+
+		@Override
+		public void onServiceConnected(ComponentName name, IBinder service) {
+			binder = (BindService.MyBinder) service;
+		}
+	};
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+
+		bind = (Button) findViewById(R.id.bind);
+		unbind = (Button) findViewById(R.id.unbind);
+		getServiceStatus = (Button) findViewById(R.id.getStatus);
+
+		final Intent intent = new Intent();
+		intent.setAction("com.example.service.BindService");
+
+		bind.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				bindService(intent, conn, Service.BIND_AUTO_CREATE);
+			}
+		});
+
+		unbind.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				unbindService(conn);
+			}
+		});
+
+		getServiceStatus.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				Toast.makeText(BindServiceTest.this,
+						"Service's count value is :" + binder.getCount(),
+						Toast.LENGTH_SHORT).show();
+			}
+		});
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.bind, menu);
+		return true;
+	}
+
+}
