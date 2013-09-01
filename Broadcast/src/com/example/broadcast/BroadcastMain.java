03783cdfe1fd41c8c108bcf710db90fc7e3fb3f1
silicon ruby <silicon@nachuantech.com>
2013-09-01 13:32:34 +0800

    refs #1 Service and BroadcastReceiver
    modify the broadcast

--- Broadcast/src/com/example/broadcast/BroadcastMain.java
+++ Broadcast/src/com/example/broadcast/BroadcastMain.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
@@ -14,24 +15,29 @@ import android.widget.TextView;
 public class BroadcastMain extends Activity {
 
 	private Button sendBroadcast;
-	private TextView receiver;
+	private TextView textview;
 
 	private static final String BROADCAST_ACTION = "com.example.action.BROADCAST";
 
-	class MyReceiver extends BroadcastReceiver {
+	private BroadcastReceiver receiver = new BroadcastReceiver() {
 
 		@Override
 		public void onReceive(Context context, Intent intent) {
-			receiver.setText(intent.getStringExtra("msg"));
+			textview.setText(intent.getStringExtra("msg"));
 		}
-	}
+	};
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
 		sendBroadcast = (Button) findViewById(R.id.sendBroadcast);
-		receiver = (TextView) findViewById(R.id.receiver);
+		textview = (TextView) findViewById(R.id.receiver);
+		// register
+		IntentFilter filter = new IntentFilter();
+		filter.addAction(BROADCAST_ACTION);
+		registerReceiver(receiver, filter);
+		// set listener for button
 		sendBroadcast.setOnClickListener(new OnClickListener() {
 
 			@Override
@@ -46,7 +52,6 @@ public class BroadcastMain extends Activity {
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.broadcast_main, menu);
 		return true;
 	}
